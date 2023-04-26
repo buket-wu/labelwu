a84e0c0d59094db8d609cc995ecaad13a3bbe2dc
wujiajin <wujiajin@sensetime.com>
2023-04-26 19:07:53 +0800

    feat: 新增更新demo功能

---
+++ scripts/upgrade-lbdemo-ver.js
@@ -0,0 +1,54 @@
+const {exec} = require('child_process');
+const fs = require('fs')
+
+let lbAnnotation = require('../packages/lb-annotation/package.json')
+let lbComponents = require('../packages/lb-components/package.json')
+let lbUtils = require('../packages/lb-utils/package.json')
+
+let demoPath = "./packages/lb-demo/package.json"
+
+let lbDemo = require('../packages/lb-demo/package.json')
+
+let newDemo = lbDemo
+
+newDemo["dependencies"]["@labelwu/lb-annotation"] = lbAnnotation["version"]
+newDemo["dependencies"]["@labelwu/lb-components"] = lbComponents["version"]
+newDemo["dependencies"]["@labelwu/lb-utils"] = lbUtils["version"]
+
+if (JSON.stringify(newDemo) === JSON.stringify(lbDemo)) {
+    console.log("upgrade success; ver not change")
+    process.exit(0)
+}
+
+fs.writeFile(demoPath, JSON.stringify(newDemo, null, "  "), function (err) {
+    if (err) {
+        console.log("upgrade fail", err)
+    } else {
+        console.log("upgrade success")
+    }
+})
+
+const gitHelper = async function () {
+    await exec('git add packages/lb-demo/package.json', (error) => {
+        if (error) {
+            console.error(`执行出错: ${error}`);
+            return;
+        }
+    });
+
+    await exec('git commit -m \"feat(ci): upgrade lb-demo dependence version\"', (error) => {
+        if (error) {
+            console.error(`执行出错: ${error}`);
+            return;
+        }
+    });
+
+    await exec('git push', (error) => {
+        if (error) {
+            console.error(`执行出错: ${error}`);
+            return;
+        }
+    });
+}
+
+gitHelper()
